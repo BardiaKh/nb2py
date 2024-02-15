efbaffbe9a02254453ffc3fa5c3053ce9cc542da
Bardia Khosravi <bardiakhosravi95@gmail.com>
2024-02-15 08:16:12 -0600

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nb2py",
-    version="0.2",
+    version="0.3",
     author="Bardia Khosravi",
     author_email="bardiakhosravi95@gmail.com",
     description="Reliable Notebook to Python converter",
